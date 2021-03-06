e43f67094d30a7eb7d15f040f5c9ad6b0eba7b3e
Mehmet Can <mehmetcy01@gmail.com>
2021-03-06 22:44:13 +0300

    first commit

---
+++ app/Http/Controllers/MockController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class MockController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+ 
+    public function purchase(Request $request)
+    {
+        $this->validate($request, [
+            'receipt' => 'required',
+        ]);
+
+        $last_char_of_receipt = substr($request->get('receipt'), -1);
+
+        $my_status = false;
+        if (is_numeric($last_char_of_receipt) && $last_char_of_receipt % 2 == 1) {
+            $my_status = true;
+        }
+
+        return response()->json([
+            'status' => true,
+            'expire-date' => Carbon::now()->add(1, 'day')->format("Y-d-m H:i:s"),
+            'receipt' => $my_status
+        ]);
+    }
+}
